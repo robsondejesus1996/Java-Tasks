79b0dd2618be5ea8ba03601a4a76d5eb03a3cb17
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-09 20:13:59 -0300

    feat: validação usuário dono

--- todolist/src/main/java/br/com/robsondejesus/todolist/task/TaksController.java
+++ todolist/src/main/java/br/com/robsondejesus/todolist/task/TaksController.java
@@ -55,14 +55,27 @@ public class TaksController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
-        var IdUser = request.getAttribute("idUser");
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id,
+            HttpServletRequest request) {
 
         var task = this.taskRepository.findById(id).orElse(null);
 
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Tarefa não encontrada");
+        }
+
+        var IdUser = request.getAttribute("idUser");
+
+        if (!task.getIdUser().equals(IdUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Usuário não tem permissão para alterar esta tarefa");
+        }
+
         Utils.copyNonNullProperties(taskModel, task);
 
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 
 }
