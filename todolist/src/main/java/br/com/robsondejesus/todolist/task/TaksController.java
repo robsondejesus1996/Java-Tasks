ef3ba50df0a0395eda08454a1a059abedd43e191
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-09 19:43:47 -0300

    feat: update parcial

--- todolist/src/main/java/br/com/robsondejesus/todolist/task/TaksController.java
+++ todolist/src/main/java/br/com/robsondejesus/todolist/task/TaksController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.robsondejesus.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -56,9 +57,12 @@ public class TaksController {
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
         var IdUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) IdUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 
 }
