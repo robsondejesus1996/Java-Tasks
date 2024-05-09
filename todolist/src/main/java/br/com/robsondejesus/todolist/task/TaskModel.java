fa5e1c21cdba18195957341e2e34d4355a4e07ea
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-09 20:46:38 -0300

    feat: live reload

--- todolist/src/main/java/br/com/robsondejesus/todolist/task/TaskModel.java
+++ todolist/src/main/java/br/com/robsondejesus/todolist/task/TaskModel.java
@@ -26,9 +26,16 @@ public class TaskModel {
     private LocalDateTime endAt;
     private String priority;
 
+    private UUID idUser;
+
     @CreationTimestamp
     private LocalDateTime createAt;
 
-    private UUID idUser;
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo title deve conter no maximo 50 caracteres");
+        }
+        this.title = title;
+    }
 
 }
